13e8debed0d398c3ebc25be27679c1f791138226
Stas Rudevitskty <stasrdv@gmail.com>
2024-04-14 10:08:31 +0300

    WIP reafctoring.

--- front-end/src/pokedex/pokeTypesSelect.tsx
+++ front-end/src/pokedex/pokeTypesSelect.tsx
@@ -1,12 +1,16 @@
 import { Select } from "antd";
 import { usePokemonTypesQuery } from "./usePokeTypesQuery";
-import { FC } from "react";
+import { FC, useMemo } from "react";
 
 export const PokemonTypesSelect: FC<{
   handleTypeSelect: (type: string) => void;
   selectedType: string;
 }> = ({ handleTypeSelect, selectedType }) => {
   const { data, error } = usePokemonTypesQuery();
+  const filteredData = useMemo(() => {
+    if (!data) return [];
+    return data.filter((type) => type.trim() !== "");
+  }, [data]);
 
   if (error) return <div>Error</div>;
 
@@ -14,7 +18,10 @@ export const PokemonTypesSelect: FC<{
     <Select
       style={{ width: 200 }}
       placeholder='Select Pokemon type'
-      options={data?.map((option) => ({ label: option, value: option }))}
+      options={filteredData?.map((option) => ({
+        label: option,
+        value: option,
+      }))}
       onChange={handleTypeSelect}
       value={selectedType ? selectedType : undefined}
       allowClear
--- front-end/src/pokedex/usePokeListParams.ts
+++ front-end/src/pokedex/usePokeListParams.ts
@@ -2,50 +2,75 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import { PokeListQueryParams } from "./types/poke-query-params";
 import { useState, useEffect } from "react";
 
+const DEFAULT_PAGE_SIZE: number = 10;
+const DEFAULT_PAGE: number = 1;
+const DEFAULT_SORT_ORDER: "asc" | "desc" = "asc";
+
 export const usePokeListQueryParams = (): PokeListParams => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
-
   const [queryParams, setQueryParams] = useState<PokeListParams["queryParams"]>(
     {
-      currentPage: parseInt(searchParams.get("page") || "1", 10),
-      pageSize: parseInt(searchParams.get("page_size") || "10", 10),
+      currentPage: parseInt(
+        searchParams.get("page") || String(DEFAULT_PAGE),
+        10
+      ),
+      pageSize: parseInt(
+        searchParams.get("page_size") || String(DEFAULT_PAGE_SIZE),
+        10
+      ),
       selectedType: searchParams.get("poke_type") || "",
-      sortOrder: searchParams.get("sort_order") === "desc" ? "desc" : "asc",
+      sortOrder:
+        searchParams.get("sort_order") === "desc" ? "desc" : DEFAULT_SORT_ORDER,
     }
   );
 
   useEffect(() => {
     setQueryParams({
-      currentPage: parseInt(searchParams.get("page") || "1", 10),
-      pageSize: parseInt(searchParams.get("page_size") || "10", 10),
+      currentPage: parseInt(
+        searchParams.get("page") || String(DEFAULT_PAGE),
+        10
+      ),
+      pageSize: parseInt(
+        searchParams.get("page_size") || String(DEFAULT_PAGE_SIZE),
+        10
+      ),
       selectedType: searchParams.get("poke_type") || "",
-      sortOrder: searchParams.get("sort_order") === "desc" ? "desc" : "asc",
+      sortOrder:
+        searchParams.get("sort_order") === "desc" ? "desc" : DEFAULT_SORT_ORDER,
     });
   }, [searchParams]);
 
   const updateSearchParams = (updatedParams: Partial<PokeListQueryParams>) => {
-    const existingParams = searchParams;
-    const updatedSearchParameters = new URLSearchParams(
-      existingParams.toString()
-    );
-    if (updatedParams.currentPage !== undefined) {
-      updatedSearchParameters.set("page", String(updatedParams.currentPage));
+    const searchParams = new URLSearchParams(location.search);
+
+    if (updatedParams.currentPage) {
+      searchParams.set("page", String(updatedParams.currentPage));
+    } else {
+      searchParams.set("page", String(DEFAULT_PAGE));
     }
-    if (updatedParams.pageSize !== undefined) {
-      updatedSearchParameters.set("page_size", String(updatedParams.pageSize));
+    if (updatedParams.pageSize) {
+      searchParams.set("page_size", String(updatedParams.pageSize));
+    } else {
+      searchParams.set("page_size", String(DEFAULT_PAGE_SIZE));
     }
-    updatedSearchParameters.set("poke_type", updatedParams.selectedType || "");
-    if (updatedParams.sortOrder !== undefined) {
-      updatedSearchParameters.set("sort_order", updatedParams.sortOrder);
+    if (updatedParams.selectedType) {
+      searchParams.set("poke_type", updatedParams.selectedType || "");
+    } else {
+      searchParams.delete("poke_type");
     }
 
-    const newSearchParamsString = updatedSearchParameters.toString();
-    const newUrl = `${window.location.pathname}${
-      newSearchParamsString ? `?${newSearchParamsString}` : ""
-    }`;
+    if (updatedParams.sortOrder) {
+      searchParams.set(
+        "sort_order",
+        updatedParams.sortOrder || DEFAULT_SORT_ORDER
+      );
+    }
 
-    navigate(newUrl, { replace: true });
+    navigate({
+      pathname: location.pathname,
+      search: searchParams.toString() || undefined,
+    });
   };
 
   return {
